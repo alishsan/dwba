67a180c5ad177149f2143fdc340f3806c950b752
Alisher Sanetullaev <alishsan@gmail.com>
2024-11-10 21:28:46 +0500

    moved functions to functions.clj

--- src/functions.clj
+++ src/functions.clj
@@ -1,35 +1,74 @@
-(ns functions)  ; Useful functions such as LEgendre polynomials
+(ns functions
+(:require
+[fastmath.core :as m]
+ [fastmath.polynomials :as poly]
+  [fastmath.special :as spec]
+))
+(use 'complex)
 
-(require '[fastmath.core :as m])
 
-(defn eval-legendre-P
-  ^double [^long degree ^double x]
-  (case (int degree)
-    0 1.0
-    1 x
-    (loop [i (long 2)
-           pprev 1.0
-           prev x]
-      (if (m/> i degree)
-        prev
-        (recur (m/inc i) prev
-               (m// (m/- (m/* (m/dec (m/* 2.0 i)) x prev)
-                         (m/* (m/dec i) pprev)) i))))))
+(defn deriv
+  ([fn1 x dx]
+   (/ (- (fn1 (+ x dx)) (fn1 x)) dx))
+  ([fn1 L x dx] ;for hankel functions with L dependence and complex numbers
+   (div (subt2 (fn1 L (+ x dx)) (fn1 L x)) dx)))
 
-(defn- legendre-P-ratio
-  [^long degree]
-  (case (int degree)
-    0 RONE
-    1 (ratio-polynomial [0 1])
-    (loop [i (long 2)
-           pprev RONE
-           prev (ratio-polynomial [0 1])]
-      (if (m/> i degree)
-        prev
-        (recur (m/inc i) prev
-               (scale (sub (mult prev (ratio-polynomial [0 (m/dec (m/* 2.0 i))]))
-                           (scale pprev (m/dec i))) (/ 1 i)))))))
 
-(defn legendre-P
-  [^long degree]
-  (polynomial (coeffs (legendre-P-ratio degree))))
+(defn distorted-wave [k r]
+  "Returns the distorted wave function at distance r for wavenumber k."
+  (complex-from-polar (* k r) (* k r)))
+
+
+(defn WS ;  Woods-Saxon potential V(R) = -V0/(1+exp ((r-R0)/a0))
+  [r [V0 R0 a0]]
+  ( / (* -1.0 V0) (+ 1.0 (Math/exp (/ (- r R0) a0))))
+  )
+
+(defn hankel0+ [L rho]
+(-  (* rho (poly/eval-bessel-t L rho))   (* rho (poly/eval-bessel-t L rho)))
+  )
+
+
+(defn hankel0- [L rho]
+(+  (* rho (poly/eval-bessel-t L rho))   (* rho (poly/eval-bessel-t L rho)))
+  )
+
+(defn r-matrix-a [^double E V  ^double a ^long L]  ;construct R-matrix * a depending on 1D Woods-Saxon potential V(R) = -V0/(1+exp ((r-R0)/a0)) V = [V0, R0, a0]
+;choose u(0) = 0, u'(0) = 1 for initial conditions
+(let [N  1000
+dr (/ a N)]
+(loop [x dr pot 0 d2udr2 (/ 1. dr) dudr 1 ur dr]
+(if (> x a)
+(/ ur dudr) ;Ra = u/dudr  (dudr = d2udr2 * dr)
+(recur  (+ x dr) (WS x V) (*  (+ (/ (* L (inc L)) (* x x)) (-  pot E)) ur) (+ dudr (* d2udr2 dr))  (+ ur (* dudr dr))) ) 
+)))
+
+
+(defn f-func [L rho] (* rho (spec/spherical-bessel-j L rho)))
+(defn g-func [L rho] (* -1 rho (spec/spherical-bessel-y L rho)))
+
+(defn hankel0+ [L rho]
+  (complex-from-cartesian (g-func L rho ) (f-func L rho))
+  )
+
+(defn hankel0- [L rho]
+  (complex-from-cartesian (g-func L rho ) (* -1.0 (f-func L rho)))
+  )
+
+
+
+(defn s-matrix [^double E V  ^double a ^long L]
+  (let [ra (r-matrix-a E V a L)]
+    (div (subt (hankel0- L a) (mul ra (deriv hankel0- L a 0.000001)) )
+         (subt (hankel0+ L a) (mul ra (deriv hankel0+ L a 0.000001)) ))
+ ))
+
+(defn k-matrix [^double E V  ^double a ^long L]
+  (let [ra (r-matrix-a E V a L)]
+    (/ (subt  (mul ra (deriv f-func L a 0.000001))  (f-func L a))
+         (subt (g-func L a) (mul ra (deriv g-func L a 0.000001)) ))
+ ))
+
+  
+
+
