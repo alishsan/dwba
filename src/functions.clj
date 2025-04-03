6d2af34531603cfeaec691f5360797bc1d55363f
Alisher Sanetullaev <alishsan@gmail.com>
2025-04-03 08:21:38 +0500

    changes

--- src/functions.clj
+++ src/functions.clj
@@ -14,7 +14,11 @@
   ([fn1 x dx]
    (/ (- (fn1 (+ x dx)) (fn1 x)) dx))
   ([fn1 L x dx] ;for hankel functions with L dependence and complex numbers
-   (div (subt2 (fn1 L (+ x dx)) (fn1 L x)) dx)))
+   (div (subt2 (fn1 L (+ x dx)) (fn1 L x)) dx))
+
+  ([fn1 L eta x dx] ;for hankel functions with L dependence and complex numbers
+   (div (subt2 (fn1 L eta (+ x dx)) (fn1 L eta x)) dx))
+)
 
 (defn to-vec2 [x] (v/vec2 (re x) (im x)))
 
@@ -45,16 +49,10 @@ dr (/ a N)]
 (recur  (+ x dr) (WS x V) (*  (+ (/ (* L (inc L)) (* x x)) (-  pot E)) ur) (+ dudr (* d2udr2 dr))  (+ ur (* dudr dr))) ) 
 )))
 
-   (defn r-matrix [^double E V ^long L ^double eta]  ;with coulomb ;construct R-matrix * a depending on 1D Coulomb + Woods-Saxon potential V(R) = -V0/(1+exp ((r-R0)/a0)) V = [V0, R0, a0]
-;choose u(0) = 0, u'(0) = 1 for initial conditions
-     (let [N  1000
-           a (* 2 (second V))
-dr (/ a N)]
-(loop [x dr pot 0 d2udr2 (/ 1. dr) dudr 1 ur dr]
-(if (> x a)
-(/ ur dudr) ;Ra = u/dudr  (dudr = d2udr2 * dr)
-(recur  (+ x dr) (WS x V) (*  (+ (/ (* L (inc L)) (* x x)) (-  pot E)) ur) (+ dudr (* d2udr2 dr))  (+ ur (* dudr dr))) ) 
-)))
+(defn Coulomb-pot [r r0] ; potential of a uniformly charged sphere of charge e and radius r0
+  (if (> r r0) (/ 1.44 r) (* r (/ 1.44 r0 r0)))
+
+  ) 
   
 
 
@@ -83,25 +81,27 @@ dr (/ a N)]
 (if (= n 1) 1 (* (factorial (- n 1)))
 ))
 
-(defn s-matrix [^double E V  ^double a ^long L]
+(defn s-matrix0 [^double E V  ^double a ^long L]
   (let [ra (r-matrix-a E V a L)]
     (div (subt2 (hankel0- L a) (mul ra (deriv hankel0- L a 0.000001)) )
          (subt2 (hankel0+ L a) (mul ra (deriv hankel0+ L a 0.000001)) ))
  ))
 
-(defn k-matrix [^double E V  ^double a ^long L]
+
+
+(defn k-matrix0 [^double E V  ^double a ^long L]
   (let [ra (r-matrix-a E V a L)]
     (div (subt2  (mul ra (deriv f-func L a 0.0000001))  (f-func L a))
          (subt2 (g-func L a) (mul ra (deriv g-func L a 0.0000001)) ))
  ))
 
-(defn phase-shift  [^double E V  ^double a ^long L ]
-  (let [s (s-matrix E V a L)]
+(defn phase-shift0  [^double E V  ^double a ^long L ]
+  (let [s (s-matrix0 E V a L)]
     (div (arg s) 2)
     ))
 
-(defn sigma-L [E V a L]
-(* (/ 2 E) Math/PI (+ (* 2 L) 1) (Math/pow (mag (subt2 1. (s-matrix E V a L)))  2) )
+(defn sigma-L0 [E V a L]
+(* (/ 2 E) Math/PI (+ (* 2 L) 1) (Math/pow (mag (subt2 1. (s-matrix0 E V a L)))  2) )
 )
 
 
@@ -168,6 +168,26 @@ dr (/ a N)]
           (cpowc (complex-from-cartesian 0 (* 2 rho)) a) 
   ( hypergeometric-complex-U a (* 2 (inc L)) (complex-from-cartesian 0 (* 2.0 rho)))
 )))
+
+   (defn r-matrix [^double E V ^long L ]  ;with coulomb ;construct R-matrix * a depending on 1D Coulomb + Woods-Saxon potential V(R) = -V0/(1+exp ((r-R0)/a0)) V = [V0, R0, a0]
+;choose u(0) = 0, u'(0) = 1 for initial conditions
+     (let [N  1000
+           R0 (second V)
+           a (* 2 R0)
+           dr (/ a N)]
+(loop [x dr pot 0 d2udr2 (/ 1. dr) dudr 1 ur dr]
+(if (> x a)
+(/ ur dudr) ;Ra = u/dudr  (dudr = d2udr2 * dr)
+(recur  (+ x dr) (+ (Coulomb-pot x R0) (WS x V)) (*  (+ (/ (* L (inc L)) (* x x)) (-  pot E)) ur) (+ dudr (* d2udr2 dr))  (+ ur (* dudr dr))) ) 
+)))
+
+(defn s-matrix [^double E V  ^double eta ^long L]
+  (let [a (* 2  (second V))
+        ra (mul a (r-matrix E V L))
+        ]
+    (div (subt2 (Hankel- L eta a) (mul ra (deriv Hankel- L a 0.000001)) )
+         (subt2 (Hankel+ L eta a) (mul ra (deriv Hankel+ L a 0.000001)) ))
+ ))
 
 (defn hypergeometric-complex-U2
 [a b z]
